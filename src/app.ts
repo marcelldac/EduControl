e4cb412a9ccbb33e59bddff39ebd797e0a18da69
marcelldac <dacmarcell@gmail.com>
2024-02-10 17:19:41 -0300

    feat: get and post student methods

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,46 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const server = express();
 const port = process.env.PORT || 3333;
 
+const prisma = new PrismaClient();
+
 server.use(cors());
 server.use(express.json());
+
+server.get("/api/v1/students", async (request: Request, response: Response) => {
+  try {
+    const students = await prisma.student.findMany();
+    return response.status(200).json({ message: students, error: false });
+  } catch (error) {
+    return response.status(500).json({ message: error, error: true });
+  }
+});
+
+server.post(
+  "/api/v1/students",
+  async (request: Request, response: Response) => {
+    try {
+      const { firstName, lastName, email, password } = request.body;
+
+      const student = {
+        firstName,
+        lastName,
+        email,
+        password,
+      };
+
+      const createStudent = await prisma.student.create({ data: student });
+      return response
+        .status(201)
+        .json({ message: createStudent, error: false });
+    } catch (error) {
+      return response.status(500).json({ message: error, error: true });
+    }
+  }
+);
 
 server.listen(port, () => {
   console.log(`App running on ${port} `);
