ffd968179dcf209d0b14fb0cdf37969a97b5cf39
marcelldac <dacmarcell@gmail.com>
2024-02-10 12:19:07 -0300

    :sparkles: feat: create web service

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 import express from "express";
 import cors from "cors";
+
+const server = express();
+const port = process.env.PORT || 3333;
+
+server.use(cors());
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log(`App running on ${port} `);
+});
