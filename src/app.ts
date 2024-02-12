60e8575fb9984f426a6148559e8f9256b6c0ba5e
marcelldac <dacmarcell@gmail.com>
2024-02-12 19:01:11 -0300

    feat: use enrollment router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import studentRouter from "./routes/student-routes";
 import courseRouter from "./routes/course-router";
+import enrollmentRouter from "./routes/enrollment-router";
 
 const server = express();
 const port = process.env.PORT || 3333;
@@ -11,6 +12,7 @@ server.use(express.json());
 
 server.use("/api/v1", studentRouter);
 server.use("/api/v1", courseRouter);
+server.use("/api/v1", enrollmentRouter);
 
 server.listen(port, () => {
   console.log(`App running on ${port}`);
