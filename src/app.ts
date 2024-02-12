c10aed0efb31bbcc5623cdcf8e634769e9e35f79
marcelldac <dacmarcell@gmail.com>
2024-02-12 11:17:13 -0300

    refactor: use student router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import studentController from "./controllers/student-controller";
+import studentRouter from "./routes/student-routes";
 
 const server = express();
 const port = process.env.PORT || 3333;
@@ -8,10 +8,7 @@ const port = process.env.PORT || 3333;
 server.use(cors());
 server.use(express.json());
 
-server.get("/api/v1/students", studentController.read);
-server.post("/api/v1/students", studentController.create);
-server.put("/api/v1/students/:id", studentController.update);
-server.delete("/api/v1/students/:id", studentController.remove);
+server.use("/api/v1", studentRouter);
 
 server.listen(port, () => {
   console.log(`App running on ${port} `);
