42106f9e53ac975f13f03099d17b3bfc579a2aea
marcelldac <dacmarcell@gmail.com>
2024-02-12 16:16:10 -0300

    feat: use courses router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import studentRouter from "./routes/student-routes";
+import courseRouter from "./routes/course-router";
 
 const server = express();
 const port = process.env.PORT || 3333;
@@ -9,6 +10,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/api/v1", studentRouter);
+server.use("/api/v1", courseRouter);
 
 server.listen(port, () => {
   console.log(`App running on ${port} `);
