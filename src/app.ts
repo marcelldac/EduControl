de2d35437b93938cf7bda2f00803d7e6cd6712e6
marcelldac <dacmarcell@gmail.com>
2024-02-13 17:02:16 -0300

    feat: add swagger docs route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+
 import studentRouter from "./routes/student-routes";
 import courseRouter from "./routes/course-router";
 import enrollmentRouter from "./routes/enrollment-router";
 import teacherRouter from "./routes/teacher-router";
+
+import swaggerDocument from "../swagger.json";
 
 const server = express();
 const port = process.env.PORT || 3333;
@@ -11,6 +15,7 @@ const port = process.env.PORT || 3333;
 server.use(cors());
 server.use(express.json());
 
+server.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 server.use("/api/v1", studentRouter);
 server.use("/api/v1", courseRouter);
 server.use("/api/v1", enrollmentRouter);
