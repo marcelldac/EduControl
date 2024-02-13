7b48a94eef2f6da4f41adf3aefa697e2e69d804b
marcelldac <dacmarcell@gmail.com>
2024-02-13 11:05:46 -0300

    feat: create teacher validation schema

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const validationSchema = z.object({
+export const studentValidationSchema = z.object({
   firstName: z.string().optional(),
   lastName: z.string().optional(),
   email: z.string().email("Email should be in a valid format").optional(),
@@ -8,4 +8,10 @@ export const validationSchema = z.object({
     .string()
     .min(8, "Password must be at least 8 characters long")
     .optional(),
+});
+
+export const teacherValidationSchema = z.object({
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  isCoordinator: z.boolean().default(false).optional(),
 });
