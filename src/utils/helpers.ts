38193a1a96440a38336ea67be1189739ec5d18e0
marcelldac <dacmarcell@protonmail.com>
2024-03-04 21:30:07 -0300

    refactor: apply solid S

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,18 +4,48 @@ import { Request, Response } from "express";
 import { JwtPayload } from "./types";
 import prisma from "../../prisma/prisma-client";
 
+//Teacher
 export const findTeacherByEmail = async (email: string) => {
   const teacher = await prisma.teacher.findUnique({
     where: { email },
   });
   return teacher;
 };
+export const findTeacherByID = async (id: string) => {
+  const teacher = await prisma.teacher.findUnique({
+    where: { id },
+  });
+  return teacher;
+};
 
+//Student
+export const findStudentByID = async (id: string) => {
+  const student = await prisma.student.findUnique({
+    where: { id },
+  });
+  return student;
+};
 export const findStudentByEmail = async (email: string) => {
   const student = await prisma.student.findUnique({
     where: { email },
   });
   return student;
+};
+
+//JWT
+export const getJwtToken = (authorization: string) => {
+  const token = authorization.split(" ")[1];
+  return token;
+};
+
+export const verifyJwt = (token: string) => {
+  const { id } = jwt.verify(token, process.env.JWT_SECRET ?? "") as JwtPayload;
+  return id;
+};
+
+export const getUserID = (token: string) => {
+  const id = verifyJwt(token);
+  return id;
 };
 
 export async function getUserProfile(req: Request, res: Response) {
@@ -26,20 +56,11 @@ export async function getUserProfile(req: Request, res: Response) {
       return res.status(401).json({ message: "Unauthorized", error: false });
     }
 
-    const token = authorization.split(" ")[1];
+    const token = getJwtToken(authorization);
+    const id = getUserID(token);
 
-    const { id } = jwt.verify(
-      token,
-      process.env.JWT_SECRET ?? ""
-    ) as JwtPayload;
-
-    const student = await prisma.student.findUnique({
-      where: { id },
-    });
-
-    const teacher = await prisma.teacher.findUnique({
-      where: { id },
-    });
+    const student = await findStudentByID(id);
+    const teacher = await findTeacherByID(id);
 
     if (!teacher && !student) {
       return res.status(401).json({ message: "Unauthorized", error: false });
