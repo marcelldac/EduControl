6c796eee7e9ab4ddaefbfeba123cab9c01793a4f
marcelldac <devmarcell@protonmail.com>
2024-04-13 13:14:00 -0300

    Refactor authentication service imports, add validation for request body, and update dependencies

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,10 +1,62 @@
 import { Request, Response } from "express";
-import { login } from "../services/auth-service";
+import {
+  getJwtToken,
+  getUserEmail,
+  login,
+  verifyJwt,
+} from "../services/auth-service";
+import Joi from "joi";
 
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  const logUser = await login(email, password);
-  return res.status(logUser.status).json(logUser.data);
+
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      message: `Erro de validação: ${error.details
+        .map((detail) => detail.message)
+        .join(",")}`,
+      error: true,
+    });
+  }
+
+  const loggedUserData = await login(email, password);
+  return res.status(loggedUserData.status).json(loggedUserData.data);
 };
 
-export default loginUser;
+export async function getUserProfile(req: Request, res: Response) {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({ message: "Unauthorized", error: false });
+    }
+
+    const token = getJwtToken(authorization);
+
+    const isValid = verifyJwt(token);
+    if (!isValid) {
+      return res.status(400).json({ message: "Invalid Token", error: true });
+    }
+
+    const email = getUserEmail(token);
+
+    return res.json({ message: email, error: false });
+  } catch (e) {
+    return res.status(500).json({ message: e, error: true });
+  }
+}
+
+//JWT
+
+const authController = {
+  loginUser,
+  getUserProfile,
+};
+
+export default authController;
