ab184851dd45fdeef2fbff2aa4e9ab84532abf7c
marcelldac <devmarcell@protonmail.com>
2024-04-13 12:18:55 -0300

    Add validation for request body in teacher-controller.ts

--- src/controllers/teacher-controller.ts
+++ src/controllers/teacher-controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import joi from "joi";
+
 import teacherService from "../services/teacher-service";
 
-export const read = async (request: Request, response: Response) => {
+export const read = async (_: Request, response: Response) => {
   try {
     const students = await teacherService.readTeacher();
     return response.status(200).json({ message: students, error: false });
@@ -12,6 +14,26 @@ export const read = async (request: Request, response: Response) => {
 export const create = async (request: Request, response: Response) => {
   try {
     const { name, email, password, isCoordinator, courseName } = request.body;
+
+    const schema = joi.object({
+      name: joi.string().required(),
+      email: joi.string().email().required(),
+      password: joi.string().required(),
+      isCoordinator: joi.boolean().default(false),
+      courseName: joi.string().required(),
+    });
+
+    const { error } = schema.validate(request.body);
+
+    if (error) {
+      return response.status(400).json({
+        message: `Erro de validação: ${error.details
+          .map((detail) => detail.message)
+          .join(",")}`,
+        error: true,
+      });
+    }
+
     const createStudent = await teacherService.createTeacher(
       { name, email, isCoordinator, password },
       courseName
